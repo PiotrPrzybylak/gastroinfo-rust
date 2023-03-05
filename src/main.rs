7d8a8f63ea0c09d3a80bc5e80fa5d6ab8de20d61
Piotr Przybylak <piotr.przybylak@gmail.com>
2023-03-05 21:24:53 +0100

    Add Handlebars support

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
 #[macro_use] extern crate rocket;
 
+use rocket_dyn_templates::{Template, context};
+
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world with Rocket!"
 }
 
+#[get("/test")]
+fn index2() -> Template {
+    Template::render("index", context! { field: "value" })
+}
+
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().attach(Template::fairing()).mount("/", routes![index, index2])
 }
