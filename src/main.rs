84aa9a34784002076b07953432f0a06eadd83ca9
Piotr Przybylak <piotr.przybylak@gmail.com>
2023-03-26 15:32:35 +0200

    Try to add postgres support

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,77 @@
 use rocket_dyn_templates::{Template, context};
 
 
+use rocket_db_pools::{Database, Connection};
+use rocket_db_pools::sqlx::{self, Row};
+
+
+#[derive(Database)]
+#[database("lanczyki")]
+struct Lanczyki(sqlx::PgPool);
+
+struct Place {
+    _id: i64,
+    name: String,
+    zone: String
+}
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world with Rocket!"
 }
 
 #[get("/test")]
-fn index2() -> Template {
-    Template::render("index", context! { field: "value" })
+async fn index2(mut db: Connection<Lanczyki>) -> Template {
+
+    // let a: Option<String> = sqlx::query("SELECT id, name, zone FROM places")
+    let ids = sqlx::query("SELECT name FROM places where id = 1")
+    .fetch_one(&mut *db).await
+    .and_then(|r| 
+        {
+            print!("duoa");
+            let z= r.try_get(0);
+
+           let x :String =  z.unwrap();
+            print!("-----{}------", x);
+            Ok(x)})
+    .ok();
+
+let dupa = ids.unwrap();
+
+print!("{}", dupa);
+
+    // let account = sqlx::query("select (1) as id, 'Herp Derpinson' as name")
+    // .fetch_one(&mut db)
+    // .await?;
+
+// print!("{}", account);
+
+        // .fetch(&mut *db)
+        // .map_ok(|record| record.id)
+        // .try_collect::<Vec<_>>()
+        // .await.unwrap();
+
+    // println!("{}", ids);
+
+
+    // for row in client.query("SELECT id, name, zone FROM places", &[]).expect("DB SELECT error") {
+    //     let place = Place {
+    //         _id: row.get(0),
+    //         name: row.get(1),
+    //         zone: row.get(2),
+    //     };
+    //     println!("Author {} is from {}", place.name, place.zone);
+    // }
+
+
+    Template::render("index", context! { field:  dupa})
 }
 
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().attach(Template::fairing()).mount("/", routes![index, index2])
+    rocket::build()
+        .attach(Template::fairing())
+        .attach(Lanczyki::init())
+        .mount("/", routes![index, index2])
 }
